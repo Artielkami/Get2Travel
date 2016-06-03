90a04bd66a26f9a48ca5323be0fef865fc91a4a6
Artiel_kami <raynortassada@gmail.com>
2016-06-03 13:54:20 +0700

    update setting

--- Get2Travel/settings.py
+++ Get2Travel/settings.py
@@ -120,10 +120,5 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = '/search/static'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/search/static/',
-]
